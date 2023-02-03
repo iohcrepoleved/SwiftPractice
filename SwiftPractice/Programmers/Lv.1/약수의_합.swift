c366a8e81e630d4bba06350ea4ff554c4d82e657
iohcrepoleved <aram829@icloud.com>
2023-02-03 13:12:51 +0900

    [수정] Lv.1 약수의 합

--- SwiftPractice/Programmers/Lv.1/약수의_합.swift
+++ SwiftPractice/Programmers/Lv.1/약수의_합.swift
@@ -5,6 +5,6 @@
 //  Created by 최아람 on 2023/02/03.
 //  https://school.programmers.co.kr/learn/courses/30/lessons/12928
 
-func solution(_ n:Int) -> Int {
+func solution1(_ n:Int) -> Int {
     return (0...n).filter{ $0 > 0 && n % $0==0}.reduce(0,+)
 }
