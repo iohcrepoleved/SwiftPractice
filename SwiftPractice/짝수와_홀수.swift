d214aeffa1e6240b4abedca8d544cb445053a932
iohcrepoleved <aram829@icloud.com>
2023-02-03 12:22:45 +0900

    test

---
+++ SwiftPractice/짝수와_홀수.swift
@@ -0,0 +1,12 @@
+//
+//  짝수와_홀수.swift
+//  SwiftPractice
+//
+//  Created by 최아람 on 2023/02/03.
+//  https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+import Foundation
+
+func solution(_ num:Int) -> String {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
