e18833a200c8cc5df304588f47b9dfe5e1b223e2
iohcrepoleved <aram829@icloud.com>
2023-02-03 12:18:40 +0900

    Initial Commit

---
+++ SwiftPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPractice
+//
+//  Created by 최아람 on 2023/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
