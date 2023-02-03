3440de80cd9018d70c837a1ae8954c5efb081da2
iohcrepoleved <aram829@icloud.com>
2023-02-03 12:20:10 +0900

    test

---
+++ SwiftPractice/약수의_합.swift
@@ -0,0 +1,10 @@
+//
+//  약수의_합.swift
+//  SwiftPractice
+//
+//  Created by 최아람 on 2023/02/03.
+// https://school.programmers.co.kr/learn/courses/30/lessons/12928
+
+func solution(_ n:Int) -> Int {
+    return (0...n).filter{ $0 > 0 && n % $0==0}.reduce(0,+)
+}
