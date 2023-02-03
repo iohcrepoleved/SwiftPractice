2df727eb37086b4ede2148ba8ff5e6bbbafb1c91
iohcrepoleved <aram829@icloud.com>
2023-02-03 13:11:31 +0900

    [수정] Lv.1 평균 구하기

--- SwiftPractice/Programmers/Lv.1/평균_구하기.swift
+++ SwiftPractice/Programmers/Lv.1/평균_구하기.swift
@@ -5,6 +5,6 @@
 //  Created by 최아람 on 2023/02/03.
 //  https://school.programmers.co.kr/learn/courses/30/lessons/12944
 
-func solution(_ arr:[Int]) -> Double {
+func solution3(_ arr:[Int]) -> Double {
     return Double(arr.reduce(0,+))/Double(arr.count)
 }
