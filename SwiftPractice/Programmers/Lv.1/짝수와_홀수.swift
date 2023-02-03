6e8b0a587342bb67bb3d573f64e772b61ffade2b
iohcrepoleved <aram829@icloud.com>
2023-02-03 13:12:24 +0900

    [수정] Lv.1 짝수와 홀수

--- SwiftPractice/Programmers/Lv.1/짝수와_홀수.swift
+++ SwiftPractice/Programmers/Lv.1/짝수와_홀수.swift
@@ -5,8 +5,6 @@
 //  Created by 최아람 on 2023/02/03.
 //  https://school.programmers.co.kr/learn/courses/30/lessons/12937
 
-import Foundation
-
-func solution(_ num:Int) -> String {
+func solution2(_ num:Int) -> String {
     return num % 2 == 0 ? "Even" : "Odd"
 }
