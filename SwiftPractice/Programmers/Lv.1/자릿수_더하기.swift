c09c128f7b31f2c3580657a7a61ef202cfd45eac
iohcrepoleved <aram829@icloud.com>
2023-02-03 13:10:55 +0900

    [수정] Lv.1 자릿수 더하기

--- SwiftPractice/Programmers/Lv.1/자릿수_더하기.swift
+++ SwiftPractice/Programmers/Lv.1/자릿수_더하기.swift
@@ -5,4 +5,6 @@
 //  Created by 최아람 on 2023/02/03.
 //  https://school.programmers.co.kr/learn/courses/30/lessons/12931
 
-
+func solution4(_ n:Int) -> Int {
+    return String(n).map{Int(String($0))!}.reduce(0,+)
+}
