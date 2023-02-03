a2ebed2c7a68afc10f396b3ae01438e93872fbc2
iohcrepoleved <aram829@icloud.com>
2023-02-03 13:03:06 +0900

    [추가] Lv.2 최댓값과 최솟값

---
+++ SwiftPractice/Programmers/Lv.2/최댓값과_최솟값.swift
@@ -0,0 +1,12 @@
+//
+//  최댓값과_최솟값.swift
+//  SwiftPractice
+//
+//  Created by 최아람 on 2023/02/03.
+//  https://school.programmers.co.kr/learn/courses/30/lessons/12939
+
+import Foundation
+
+func solution(_ s:String) -> String {
+    return "\(s.split(separator:" ").map{Int($0)!}.min()!) \(s.split(separator:" ").map{Int($0)!}.max()!)"
+}
