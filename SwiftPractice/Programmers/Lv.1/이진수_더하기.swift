a642df7774af3c086c671f8e80a68e67044db4de
iohcrepoleved <aram829@icloud.com>
2023-02-07 21:03:58 +0900

    [추가] Lv.1 이진수 더하기

---
+++ SwiftPractice/Programmers/Lv.1/이진수_더하기.swift
@@ -0,0 +1,13 @@
+//
+//  이진수_더하기.swift
+//  SwiftPractice
+//
+//  Created by 최아람 on 2023/02/07.
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120885
+
+import Foundation
+func solution(_ bin1:String, _ bin2:String) -> String {
+    var dec1 = Int(bin1, radix:2) ?? 0
+    var dec2 = Int(bin2, radix:2) ?? 0
+    return String(dec1 + dec2, radix:2)
+}
